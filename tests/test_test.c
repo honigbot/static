1bcec7bd7be27a89c44b8ee3c2ce225d6738708d
softbaers thesoftbear@gmail.com <thesoftbear@gmail.com>
2022-11-21 03:51:35 +0100

    fix file ending warnings

--- tests/test_test.c
+++ tests/test_test.c
@@ -2,115 +2,103 @@
 #include <stdio.h>
 #include <unistd.h>
 
-FILE * report = NULL;
-const char * current_file = NULL;
-int current_line = 0;
-const char * current_expression = NULL;
+typedef struct test_assertion_t {
+    const char * name;
+    const char * file;
+    const char * expression;
+    int line;
+} test_assertion_t;
 
-#ifndef STATIC_TEST_EXECUTE_MESSAGE
-    #define STATIC_TEST_EXECUTE_MESSAGE(name) {\
-        fprintf(stderr, "\033[1m⬜ EXECUTING %s()\033[0m\n", current_name);\
-    }
-#endif
-
-#ifndef STATIC_TEST_SUCCESS_MESSAGE
-    #define STATIC_TEST_SUCCESS_MESSAGE(name) {\
-        fprintf(stderr, "\033[1;32m✅ PASSED %s()\033[0m\n", name);\
-    }
-#endif
-
-#ifndef STATIC_TEST_FAILURE_MESSAGE
-    #define STATIC_TEST_FAILURE_MESSAGE(name) {\
-        fprintf(stderr, "\033[1;31m⛔ FAILED %s():\033[0m %s:%d: \n    %s\n", name, current_file, current_line, current_expression);\
-    }
-#endif
-
-int STATIC_execute(const char* current_name, void (*current_function)()) {
-    STATIC_TEST_EXECUTE_MESSAGE(current_name);
-	current_function();
-	if(current_file == NULL) {
-        STATIC_TEST_SUCCESS_MESSAGE(current_name);
-		return 0;
-	} else {
-        STATIC_TEST_FAILURE_MESSAGE(current_name);
-		return -1;
-	}
-}
-
-int STATIC_ASSERT_(const char * file, int line, const char * expression, int pass) {
-	if(pass) return 1;
-	current_file = file;
-	current_line = line;
-	current_expression = expression;
-	return 0;
-}
-
-// #define STATIC_TEST(function) if(STATIC_execute((#function), function) == -1) return -1;
-
-#define STATIC_ASSERT(expression) if (!STATIC_ASSERT_(__FILE__, __LINE__, (#expression), (expression) ? 1 : 0)) return
-#define STATIC_ASSERT_EQ(expected, actual) STATIC_ASSERT(expected == actual)
-#define STATIC_ASSERT_NE(expected, actual) STATIC_ASSERT(expected != actual)
-#define STATIC_ASSERT_GE(expected, actual) STATIC_ASSERT(expected >= actual)
-#define STATIC_ASSERT_LE(expected, actual) STATIC_ASSERT(expected <= actual)
-#define STATIC_ASSERT_GT(expected, actual) STATIC_ASSERT(expected > actual)
-#define STATIC_ASSERT_LT(expected, actual) STATIC_ASSERT(expected < actual)
-#define STATIC_ASSERT_EXIT
-
-#include <stdlib.h>
-
-typedef struct test_t {
+typedef struct test_case_t {
     const char * name;
     int result;
     void (*function)();
-    struct test_t * next_test;
-} test_t;
+} test_case_t;
 
 typedef struct test_suite_t {
     const char * name;
     size_t tests;
-    test_t * first_test;
     FILE * file;
 } test_suite_t;
 
 test_suite_t test_suite;
+test_case_t test_case;
+test_assertion_t test_assertion;
 
 void write_report(const test_suite_t * suite) {
     FILE * f = fopen("report.xml", "w");
     fprintf(f,
 "<testsuite name=\"%s\">\n\
-    <testcase classname=\"a\" name=\"ASuccessfulTest\"/>\n\
-    <testcase classname=\"b\" name=\"AnotherSuccessfulTest\"/>\n\
-    <testcase name=\"fail\">\n\
+    <testcase id=\"a\" name=\"ASuccessfulTest\"/>\n\
+    <testcase id=\"b\" name=\"AnotherSuccessfulTest\"/>\n\
+    <testcase id=\"fail\">\n\
         <failure type=\"ASSERT_EQ\">/Users/Maximilian/Desktop/🗿 static/tests/test_test.c:57: 1 == 2</failure>\n\
     </testcase>\n\
 </testsuite>"\
-    , suite->name, suite->tests);
+    , suite->name);
     fclose(f);
 }
 
-#define STATIC_TEST_SUITE(test_suite_name) {\
-    test_suite.name = #test_suite_name;\
-    test_suite.tests = 0;\
-    test_suite_name();\
-    write_report(&test_suite);\
+void report_test_suite_open() {
+    test_suite.file = fopen("report.xml", "w");
+    fprintf(test_suite.file, "<testsuite name=\"%s\">\n", test_suite.name);
 }
 
-#define STATIC_TEST(function) {\
-    test_t test; \
-    test.name = #function; \
-    test.result = 0;\
-    test_suite.tests += 1;\
+void report_test_suite_close() {
+    fprintf(test_suite.file, "</testsuite>");
+    fclose(test_suite.file);
 }
 
-/*
-<testsuite name="main_suite" tests="3">
-    <testcase classname="foo1" name="ASuccessfulTest"/>
-    <testcase classname="foo2" name="AnotherSuccessfulTest"/>
-    <testcase name="fail">
-        <failure type="ASSERT_EQ">/Users/Maximilian/Desktop/🗿 static/tests/test_test.c:57: 1 == 2</failure>
-    </testcase>
-</testsuite>
-*/
+void static_test_suite(const char * name, void (*function)()) {
+    test_suite.name = name;
+    test_suite.tests = 0;
+    report_test_suite_open();
+    function();
+    report_test_suite_close();
+}
+
+#define STATIC_TEST_SUITE(test_suite_name) static_test_suite(#test_suite_name, test_suite_name)
+
+void static_test_failure() {
+    fprintf(stderr, "\033[1;31m⛔ FAILED %s():\033[0m %s:%d: \n    %s\n", test_case.name, test_assertion.file, test_assertion.line, test_assertion.expression);
+    fprintf(test_suite.file, 
+    "    <testcase name=\"%s()\"/>\n"
+    "        <failure type=\"%s\">%s:%d:    %s</failure>\n"
+    "    </testcase>\n",
+        test_case.name, test_assertion.name, test_assertion.file, test_assertion.line, test_assertion.expression);
+}
+
+void static_test_success() {
+    fprintf(stderr, "\033[1;32m✅ PASSED %s()\033[0m\n", test_case.name);
+    fprintf(test_suite.file, "    <testcase name=\"%s()\"/>\n", test_case.name);
+}
+
+void static_test(const char * name, void (*function)()) {
+    test_case.name = name;
+    test_case.result = 0;
+    test_suite.tests += 1;
+    fprintf(stderr, "\033[1m⬜ EXECUTING %s()\033[0m\n", test_case.name );
+	function();
+	if(test_assertion.file == NULL) {
+        static_test_success();
+	} else {
+        static_test_failure();
+	}
+}
+
+#define STATIC_TEST(function) static_test(#function, function)
+
+void static_log_assert(const char * name, const char * file, int line, const char * expression) {
+    test_assertion.name = name;
+	test_assertion.file = file;
+	test_assertion.line = line;
+	test_assertion.expression = expression;
+}
+
+#define STATIC_ASSERT_(name, expression) if(!(expression)) { static_log_assert(name, __FILE__, __LINE__, #expression); return; }
+#define STATIC_ASSERT(expression) STATIC_ASSERT_("ASSERT", expression)
+#define STATIC_ASSERT_EQ(expected, actual) STATIC_ASSERT_("ASSERT_EQ", expected == actual)
+
 
 void success() {
     STATIC_ASSERT_EQ(1, 1);
@@ -122,6 +110,12 @@ void fail() {
 
 void main_suite() {
     STATIC_TEST(success);
+    STATIC_TEST(fail);
+    STATIC_TEST(success);
+    STATIC_TEST(success);
+    STATIC_TEST(success);
+    STATIC_TEST(fail);
+    STATIC_TEST(fail);
     STATIC_TEST(fail);
 }
 
