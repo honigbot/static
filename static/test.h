d6bb268e76a3403f86beea03d49c1a2545c4bca9
softbaers thesoftbear@gmail.com <thesoftbear@gmail.com>
2022-11-22 11:41:34 +0100

    fix missing include

--- static/test.h
+++ static/test.h
@@ -1,8 +1,9 @@
 #ifndef STATIC_TEST_H
 #define STATIC_TEST_H
 #include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
+#include <stdint.h>
+#include <string.h>
 #include <unistd.h>
 #include <time.h>
 
