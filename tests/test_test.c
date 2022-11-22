f54d142d921ed3b2f69b21a460f0302cf434d6a7
softbaers thesoftbear@gmail.com <thesoftbear@gmail.com>
2022-11-22 11:39:53 +0100

    update tests for updated functions

--- tests/test_test.c
+++ tests/test_test.c
@@ -1,29 +1,39 @@
 #define STATIC_TEST_REPORT "report.xml"
 #define STATIC_TEST_PRINT stderr
+
+#define TEST_REPORT_PRINT stderr
+#define TEST_REPORT_FILE "report.xml"
+
 #include "static/test.h"
 
 void success() {
-    STATIC_ASSERT_EQ(1, 1);
+    STATIC_ASSERT(1 == 1);
 }
 
 void fail() {
-    STATIC_ASSERT_EQ(1, 2);
+    STATIC_ASSERT_MESSAGE(1 == 2, "1 should be equal 2");
 }
 
-void test_suite1() {
-    STATIC_TEST_CASE(success);
-    STATIC_TEST_CASE(fail);
+
+void test_suite_1() {
     STATIC_TEST_CASE(success);
     STATIC_TEST_CASE(fail);
 }
 
-void test_suite2() {
-    STATIC_TEST_CASE(success);
+void test_suite_2() {
+    STATIC_TEST_CASE(fail);
     STATIC_TEST_CASE(success);
     STATIC_TEST_CASE(fail);
     STATIC_TEST_CASE(fail);
+    STATIC_TEST_CASE(success);
+    STATIC_TEST_CASE(success);
+}
+
+void test_run() {
+    STATIC_TEST_SUITE(test_suite_1);
+    STATIC_TEST_SUITE(test_suite_2);
 }
 
 int main() {
-    STATIC_TEST_SUITE(test_suite1);
+    STATIC_TEST_RUN(test_run);
 }
