c3e214fc29044b52b23f848b5d1b993cbeb0649d
softbaers thesoftbear@gmail.com <thesoftbear@gmail.com>
2022-11-28 05:47:01 +0100

    remove prefix from log

--- static/log.h
+++ static/log.h
@@ -8,105 +8,105 @@ extern "C" {
 #include <string.h>
 #include <time.h>
 
-#define STATIC_LOG_LEVEL_TRACE 0
-#define STATIC_LOG_LEVEL_DEBUG 1
-#define STATIC_LOG_LEVEL_INFO 2
-#define STATIC_LOG_LEVEL_WARN 3
-#define STATIC_LOG_LEVEL_ERROR 4
-#define STATIC_LOG_LEVEL_FATAL 5
-#define STATIC_LOG_LEVEL_NONE 6
+#define LOG_LEVEL_TRACE 0
+#define LOG_LEVEL_DEBUG 1
+#define LOG_LEVEL_INFO 2
+#define LOG_LEVEL_WARN 3
+#define LOG_LEVEL_ERROR 4
+#define LOG_LEVEL_FATAL 5
+#define LOG_LEVEL_NONE 6
 
-#define STATIC_LOG_FORMAT_(function, message, size, ...) {\
-    int position = function(message+size, STATIC_LOG_BUFFER_SIZE-size, __VA_ARGS__);\
+#define LOG_FORMAT_(function, message, size, ...) {\
+    int position = function(message+size, LOG_BUFFER_SIZE-size, __VA_ARGS__);\
     if(position >= 0) size += position; \
 }
 
-#ifndef STATIC_LOG_FORMAT_PREFIX
-    #define STATIC_LOG_FORMAT_PREFIX(message, size, level, file, function, line, ...) {\
+#ifndef LOG_FORMAT_PREFIX
+    #define LOG_FORMAT_PREFIX(message, size, level, file, function, line, ...) {\
         static const char * prefix[] = { "TRACE", "DEBUG", "INFO ", "WARN ", "ERROR", "FATAL" };\
-        STATIC_LOG_FORMAT_(snprintf, message, size, "%s", prefix[level]);\
+        LOG_FORMAT_(snprintf, message, size, "%s", prefix[level]);\
     }
 #endif
 
-#ifndef STATIC_LOG_FORMAT_TIME
-    #define STATIC_LOG_FORMAT_TIME(message, size, level, file, function, line, ...) {\
+#ifndef LOG_FORMAT_TIME
+    #define LOG_FORMAT_TIME(message, size, level, file, function, line, ...) {\
         struct timespec time_spec;\
         clock_gettime(CLOCK_REALTIME, &time_spec);\
         struct tm * time_info = localtime(&time_spec.tv_sec);\
-        STATIC_LOG_FORMAT_(strftime, message, size, "%d.%m.%Y %H:%M:%S:", time_info);\
-        STATIC_LOG_FORMAT_(snprintf, message, size, "%.3ld", time_spec.tv_nsec / 1000000);\
+        LOG_FORMAT_(strftime, message, size, "%d.%m.%Y %H:%M:%S:", time_info);\
+        LOG_FORMAT_(snprintf, message, size, "%.3ld", time_spec.tv_nsec / 1000000);\
     }
 #endif
 
-#ifndef STATIC_LOG_FORMAT_LOCATION
-    #define STATIC_LOG_FORMAT_LOCATION(message, size, level, file, function, line, ...) {\
+#ifndef LOG_FORMAT_LOCATION
+    #define LOG_FORMAT_LOCATION(message, size, level, file, function, line, ...) {\
         const char * last_slash = strrchr(file, '/');\
         const char * file_name = last_slash ? last_slash + 1 : file;\
-        STATIC_LOG_FORMAT_(snprintf, message, size, "%s:%s:%d", file_name, function, line);\
+        LOG_FORMAT_(snprintf, message, size, "%s:%s:%d", file_name, function, line);\
     }
 #endif 
 
-#ifndef STATIC_LOG_FORMAT_MESSAGE
-    #define STATIC_LOG_FORMAT_MESSAGE(message, size, level, file, function, line, ...) {\
-        STATIC_LOG_FORMAT_PREFIX(message, size, level, file, function, line, __VA_ARGS__);\
-        STATIC_LOG_FORMAT_(snprintf, message, size, " ");\
-        STATIC_LOG_FORMAT_TIME(message, size, level, file, function, line, __VA_ARGS__);\
-        STATIC_LOG_FORMAT_(snprintf, message, size, " ");\
-        STATIC_LOG_FORMAT_LOCATION(message, size, level, file, function, line, __VA_ARGS__);\
-        STATIC_LOG_FORMAT_(snprintf, message, size, " ");\
-        STATIC_LOG_FORMAT_(snprintf, message, size, __VA_ARGS__);\
+#ifndef LOG_FORMAT_MESSAGE
+    #define LOG_FORMAT_MESSAGE(message, size, level, file, function, line, ...) {\
+        LOG_FORMAT_PREFIX(message, size, level, file, function, line, __VA_ARGS__);\
+        LOG_FORMAT_(snprintf, message, size, " ");\
+        LOG_FORMAT_TIME(message, size, level, file, function, line, __VA_ARGS__);\
+        LOG_FORMAT_(snprintf, message, size, " ");\
+        LOG_FORMAT_LOCATION(message, size, level, file, function, line, __VA_ARGS__);\
+        LOG_FORMAT_(snprintf, message, size, " ");\
+        LOG_FORMAT_(snprintf, message, size, __VA_ARGS__);\
     }
 #endif
 
-#ifndef STATIC_LOG_LEVEL
+#ifndef LOG_LEVEL
     #define LOG_LEVEL LOG_LEVEL_TRACE
 #endif
 
-#ifndef STATIC_LOG_BUFFER_SIZE
-    #define STATIC_LOG_BUFFER_SIZE 128
+#ifndef LOG_BUFFER_SIZE
+    #define LOG_BUFFER_SIZE 128
 #endif
 
-#define STATIC_LOG_START(level, file, function, line, ...) {\
+#define LOG_START(level, file, function, line, ...) {\
     int _log_size=0;\
-    static char _log_message[STATIC_LOG_BUFFER_SIZE];\
-    STATIC_LOG_FORMAT_MESSAGE(_log_message, _log_size, level, file, function, line, __VA_ARGS__);\
+    static char _log_message[LOG_BUFFER_SIZE];\
+    LOG_FORMAT_MESSAGE(_log_message, _log_size, level, file, function, line, __VA_ARGS__);\
     fprintf(stderr, "%s\n", _log_message);\
 }
         
-#if STATIC_LOG_LEVEL <= STATIC_LOG_LEVEL_FATAL
-    #define STATIC_LOG_FATAL(...) STATIC_LOG_START(STATIC_LOG_LEVEL_FATAL, __FILE__, __func__, __LINE__, __VA_ARGS__)
+#if LOG_LEVEL <= LOG_LEVEL_FATAL
+    #define LOG_FATAL(...) LOG_START(LOG_LEVEL_FATAL, __FILE__, __func__, __LINE__, __VA_ARGS__)
 #else
-    #define STATIC_LOG_FATAL(...) 
+    #define LOG_FATAL(...) 
 #endif 
 
-#if STATIC_LOG_LEVEL <= STATIC_LOG_LEVEL_ERROR
-    #define STATIC_LOG_ERROR(...) STATIC_LOG_START(STATIC_LOG_LEVEL_ERROR, __FILE__, __func__, __LINE__, __VA_ARGS__)
+#if LOG_LEVEL <= LOG_LEVEL_ERROR
+    #define LOG_ERROR(...) LOG_START(LOG_LEVEL_ERROR, __FILE__, __func__, __LINE__, __VA_ARGS__)
 #else
-    #define STATIC_LOG_ERROR(...) 
+    #define LOG_ERROR(...) 
 #endif 
 
-#if STATIC_LOG_LEVEL <= STATIC_LOG_LEVEL_WARN
-    #define STATIC_LOG_WARN(...) STATIC_LOG_START(STATIC_LOG_LEVEL_WARN, __FILE__, __func__, __LINE__, __VA_ARGS__)
+#if LOG_LEVEL <= LOG_LEVEL_WARN
+    #define LOG_WARN(...) LOG_START(LOG_LEVEL_WARN, __FILE__, __func__, __LINE__, __VA_ARGS__)
 #else
-    #define STATIC_LOG_WARN(...) 
+    #define LOG_WARN(...) 
 #endif 
 
-#if STATIC_LOG_LEVEL <= STATIC_LOG_LEVEL_INFO
-    #define STATIC_LOG_INFO(...) STATIC_LOG_START(STATIC_LOG_LEVEL_INFO, __FILE__, __func__, __LINE__, __VA_ARGS__)
+#if LOG_LEVEL <= LOG_LEVEL_INFO
+    #define LOG_INFO(...) LOG_START(LOG_LEVEL_INFO, __FILE__, __func__, __LINE__, __VA_ARGS__)
 #else
-    #define STATIC_LOG_INFO(...) 
+    #define LOG_INFO(...) 
 #endif 
 
-#if STATIC_LOG_LEVEL <= STATIC_LOG_LEVEL_DEBUG
-    #define STATIC_LOG_DEBUG(...) STATIC_LOG_START(STATIC_LOG_LEVEL_DEBUG, __FILE__, __func__, __LINE__, __VA_ARGS__)
+#if LOG_LEVEL <= LOG_LEVEL_DEBUG
+    #define LOG_DEBUG(...) LOG_START(LOG_LEVEL_DEBUG, __FILE__, __func__, __LINE__, __VA_ARGS__)
 #else
-    #define STATIC_LOG_DEBUG(...) 
+    #define LOG_DEBUG(...) 
 #endif 
 
-#if STATIC_LOG_LEVEL <= STATIC_LOG_LEVEL_TRACE
-    #define STATIC_LOG_TRACE(...) STATIC_LOG_START(STATIC_LOG_LEVEL_TRACE, __FILE__, __func__, __LINE__, __VA_ARGS__)
+#if LOG_LEVEL <= LOG_LEVEL_TRACE
+    #define LOG_TRACE(...) LOG_START(LOG_LEVEL_TRACE, __FILE__, __func__, __LINE__, __VA_ARGS__)
 #else
-    #define STATIC_LOG_TRACE(...) 
+    #define LOG_TRACE(...) 
 #endif 
 
 #ifdef __cplusplus
--- tests/log_custom.c
+++ tests/log_custom.c
@@ -1,16 +1,16 @@
-#define STATIC_LOG_FORMAT_PREFIX(message, size, level, file, function, line, format, ...) {\
+#define LOG_FORMAT_PREFIX(message, size, level, file, function, line, format, ...) {\
     static const char * prefix[] = { "👣", "🐞", "💬", "⚠️ ", "💥", "💀" };\
-    STATIC_LOG_FORMAT_(snprintf, message, size, "%s", prefix[level]);\
+    LOG_FORMAT_(snprintf, message, size, "%s", prefix[level]);\
 }
 
 #include "static/log.h"
 
 int main() {
-    STATIC_LOG_TRACE("trace %d", 1);
-    STATIC_LOG_DEBUG("debug %d", 2);
-    STATIC_LOG_INFO("info %d", 3);
-    STATIC_LOG_WARN("warn %d", 4);
-    STATIC_LOG_ERROR("error %d", 5);
-    STATIC_LOG_FATAL("fatal %d", 6);
+    LOG_TRACE("trace %d", 1);
+    LOG_DEBUG("debug %d", 2);
+    LOG_INFO("info %d", 3);
+    LOG_WARN("warn %d", 4);
+    LOG_ERROR("error %d", 5);
+    LOG_FATAL("fatal %d", 6);
     return 0;
 }
